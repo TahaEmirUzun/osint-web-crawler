820169e08aae8f633df62a99875bae58837a06fe
Taha Emir Uzun <tahaemiruzun@gmail.com>
2026-08-13 20:31:12 +0300

    #feat: configure CORS middleware and environment variables securely

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 # Yeni veritabanı bağlantı ve tabloları başlatma fonksiyonumuzu alıyoruz
 from app.database.connection import init_db
@@ -8,6 +10,8 @@ from app.database.connection import init_db
 from app.scheduler import start_scheduler, scheduler  
 
 from app.api.routes import health, sources , crawled_data, statistics , logs , crawls
+
+
 
 # Uygulama açılırken ve kapanırken ne olacağını belirleyen sistem
 @asynccontextmanager
@@ -20,12 +24,24 @@ async def lifespan(app: FastAPI):
 
 # lifespan'i FastAPI uygulamamıza tanımlıyoruz
 app = FastAPI(
-    title="OSINT Security Advisory Crawler API", 
+    title=os.getenv("APP_NAME", "OSINT Security Advisory Crawler API"),
     description="Siber Güvenlik Zafiyet ve Duyuru Tarayıcısı",
     lifespan=lifespan
 )
 
-# Veritabanı tablolarını fiziksel olarak oluşturur (Yeni yazdığımız yapıyı kullanıyoruz)
+# CORS (Cross-Origin Resource Sharing) Ayarları 
+# React veya Vue gibi frontend uygulamalarının bu API'ye erişmesine izin veriyoruz
+origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # Sadece izin verilen adresler (Örn: localhost:3000)
+    allow_credentials=True,
+    allow_methods=["*"],         
+    allow_headers=["*"],         
+)
+
+# Veritabanı tablolarını fiziksel olarak oluşturur ve gerekli başlangıç verilerini ekler
 init_db()
 
 # Çalışan rotalarımızı (endpoints) dahil ediyoruz
